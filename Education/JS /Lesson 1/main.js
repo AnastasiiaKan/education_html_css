918bf795e18be4dfd727558e6b335e8fc1e0c9c0
AnastasiiaKan <nastiakanarsky@gmail.com>
2024-12-20 22:42:16 +0200

    html all homeworks and JS1

---
+++ Education/JS /Lesson 1/main.js
@@ -0,0 +1,91 @@
+// let a1 = "hello";
+// let a2 = "owu";
+// let a3 = `com`;
+// let a4 = `ua`;
+// let a5 = `1`;
+// let a6 = `10`;
+// let a7 = `-999`;
+// let a8 = `123`;
+// let a9 = `3.14`;
+// let a10 = `2.7`;
+// let a11 = `16`;
+// let a12 = `true`;
+// let a13 = `false`;
+
+// console.log(a1);
+// console.log(a2);
+// console.log(a3);
+// console.log(a4);
+// console.log(a5);
+// console.log(a6);
+// console.log(a7);
+// console.log(a8);
+// console.log(a9);
+// console.log(a10);
+// console.log(a11);
+// console.log(a12);
+// console.log(a13);
+
+// document.write(`
+//   <div>
+//     ${a1} ${a2}
+//   </div>`);
+// document.write(`
+//     <div>
+//       ${a1} ${a2}
+//     </div>`);
+// document.write(a2);
+// document.write(a3);
+// document.write(a4);
+// document.write(a5);
+// document.write(a6);
+// document.write(a7);
+// document.write(a8);
+// document.write(a9);
+// document.write(a10);
+// document.write(a11);
+// document.write(a12);
+// document.write(a13);
+
+// alert(`hello`);
+// alert(`owu.com.ua`);
+
+// let book = {
+//   title: `Harry Potter`,
+//   pages: 400,
+//   genre: `fantasy`,
+//   author: `J. K. Rowling`,
+// };
+
+// console.log(book);
+
+// let book = {
+//   title: `hgbdhbfd`,
+//   pages: 567,
+//   genre: `hgdbhg`,
+//   authors: ["hdgjh", "ghk", "hgjlh"],
+// };
+// // console.log(book.authors[1]);
+// console.log(`${book.pages}, ${book.authors[2]}`);
+
+// let FirstName = `Anatasiia`;
+// let MN = `Andriivna`;
+// let LastName = `Kanarska`;
+// let person = `${FirstName} ${MN} ${LastName}`;
+// console.log(person);
+
+// let FirstName = `Anatasiia`;
+// let MN = `Andriivna`;
+// let LastName = `Kanarska`;
+// let person = FirstName + ` ` + MN + ` ` + LastName;
+// console.log(person);
+
+// let FirstName = prompt(`enter your first name`);
+// let LastName = prompt(`enter your last name`);
+// let age = prompt(`enter your age`);
+// let person = FirstName + ` ` + LastName + ` ` + age;
+// console.log(person);
+let a1 = 100;
+let a2 = `100`;
+let a3 = true;
+console.log(typeof a3);
---
+++ Education/JS /Lesson 2/main.js
@@ -0,0 +1,50 @@
+// let a = prompt("enter a number");
+// console.log(typeof a);
+
+// if (a == 0) {
+//   console.log("yes");
+// } else if (a == 1) {
+//   console.log("lalalalala");
+// } else {
+//   console.log("no");
+// }
+// let time = +prompt("enter time");
+// if (time >= 0 && time <= 15) {
+//   console.log("first quarter");
+// } else if (time >= 16 && time <= 30) {
+//   console.log("second quarter");
+// } else if (time >= 31 && time <= 45) {
+//   console.log("third quarter");
+// } else if (time >= 46 && time <= 59) {
+//   console.log("fourth quarter");
+// } else {
+//   console.log("huinya");
+// }
+// let day = prompt("enter day");
+// switch (day) {
+//   case "1":
+//     console.log("monday");
+//     break;
+//   case "2":
+//     console.log("tuesday");
+//     break;
+//   case "3":
+//     console.log("wednesday");
+//     break;
+//   case "4":
+//     console.log("thursday");
+//     break;
+//   default:
+//     console.log("huinya");
+// }
+let a = "huinia";
+let b = 5;
+if (a > b) {
+  console.log(a);
+} else if (b > a) {
+  console.log(b);
+} else if (a == b) {
+  console.log("equal");
+} else {
+  console.log("???");
+}
